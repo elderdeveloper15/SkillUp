afc3955ff7fcf22b389b5d5286e6748c85b28473
elderdeveloper15 <jesusangel16300077@gmail.com>
2023-05-02 21:54:15 -0600

    ROUTING

--- skillup/src/app/app-routing.module.ts
+++ skillup/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+import { LandingComponent } from './landing/landing.component';
+import { RegistroempresaComponent } from './registroempresa/registroempresa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'iniciosesion',
+    component:IniciosesionComponent
+  },
+  {
+    path:'registroempresa',
+    component:LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
