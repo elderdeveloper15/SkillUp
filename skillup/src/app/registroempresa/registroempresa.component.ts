d9abe487c1876ba98f3f9afee53d72b2f418b131
nicoldesiret <nidesire25@gmail.com>
2023-05-08 21:19:34 -0600

    comando registroempresa

---
+++ skillup/src/app/registroempresa/registroempresa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registroempresa',
+  templateUrl: './registroempresa.component.html',
+  styleUrls: ['./registroempresa.component.css']
+})
+export class RegistroempresaComponent {
+
+}
