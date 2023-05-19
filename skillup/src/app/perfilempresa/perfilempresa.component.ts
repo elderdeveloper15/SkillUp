84afbb4fc5ab12f37d332b5e3cb1f9143d43e073
Andrade Larios Pablo Ivan <131845296+ivanlarios22@users.noreply.github.com>
2023-05-18 23:39:32 -0600

    Empezando a unir la API en p.em y p.es

--- skillup/src/app/perfilempresa/perfilempresa.component.ts
+++ skillup/src/app/perfilempresa/perfilempresa.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfilempresa',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class PerfilempresaComponent {
 
+    constructor(private http:HttpClient, private router: Router){}
+    
 }
--- skillup/src/app/perfilestudiante/perfilestudiante.component.ts
+++ skillup/src/app/perfilestudiante/perfilestudiante.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfilestudiante',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfilestudiante.component.css']
 })
 export class PerfilestudianteComponent {
+
+  constructor(private http:HttpClient, private router: Router){}
 
 }
